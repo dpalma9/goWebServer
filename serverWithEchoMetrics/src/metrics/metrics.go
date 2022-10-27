436fcb30db361b057b5774e3d72520d0ee838c40
U2DS4903 <dpalma@externos.itnow.es>
2022-10-27 17:20:17 +0100

    web server prometheus metrics echo framework

---
+++ serverWithEchoMetrics/src/metrics/metrics.go
@@ -0,0 +1,25 @@
+package metrics
+
+//import "fmt"
+import (
+	"github.com/labstack/gommon/log"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+func Test() string {
+  // Prometheus
+  myCounter := prometheus.NewGauge(prometheus.GaugeOpts{
+  	Name:        "my_handler_executions",
+  	Help:        "Counts executions of my handler function.",
+  	ConstLabels: prometheus.Labels{"version": "1234"},
+  })
+  if err := prometheus.Register(myCounter); err != nil {
+  	log.Fatal(err)
+  }
+  //
+  //fmt.Println("Mensaje de prueba")
+  var texto string
+  texto = "MENSAJE DE PRUEBA"
+  myCounter.Set(9.9)
+  return texto
+}
---
+++ serverWithEchoMetrics/src/server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	promMW "github.com/labstack/echo-contrib/prometheus"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+	//"github.com/prometheus/client_golang/prometheus"
+	"net/http"
+
+	"myweb/metrics"
+)
+
+func main() {
+	e := echo.New()
+	p := promMW.NewPrometheus("echo", nil)
+
+	//myCounter := prometheus.NewGauge(prometheus.GaugeOpts{
+	//	Name:        "my_handler_executions",
+	//	Help:        "Counts executions of my handler function.",
+	//	ConstLabels: prometheus.Labels{"version": "1234"},
+	//})
+	//if err := prometheus.Register(myCounter); err != nil {
+	//	log.Fatal(err)
+	//}
+
+	e.GET("/", func(c echo.Context) error {
+		//myCounter.Inc()
+		//return c.JSON(http.StatusOK, "OK")
+		return c.JSON(http.StatusOK, metrics.Test())
+	})
+
+	p.Use(e) // Enable metrics middleware and register route to expose metrics
+
+	if err := e.Start("localhost:8080"); err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+}
